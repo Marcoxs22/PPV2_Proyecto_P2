cb57b0b9822174f65e84666638c55ad548974cca
Marcoxs22 <marfogicano@gmail.com>
2024-03-13 01:20:56 -0600

    Adelanto Proyecto

--- PPV2_Proyecto_Parcial2/Assets/Scripts/LessonContainer.cs
+++ PPV2_Proyecto_Parcial2/Assets/Scripts/LessonContainer.cs
@@ -19,6 +19,9 @@ public class LessonContainer : MonoBehaviour
     [Header("External GameObject Configuration")]
     public GameObject lessonContainer;
 
+    [Header("Lesson Data")]
+    public ScriptableObject LessonData;
+
 
     //// Start is called before the first frame update
     void Start()
---
+++ PPV2_Proyecto_Parcial2/Assets/Scripts/Subject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Subject", menuName = "ScriptableObjects/NewLesson", order = 1)]
+
+public class Subject : ScriptableObject
+{
+    [Header("GameObject Configuration")]
+    public int Lessson = 0;
+
+    [Header("Lesson Quest Configuration")]
+    public List<Leccion> leccionList;
+}
