712cd871ea17df441d0703e28ed5663063fcb9b1
Marcoxs22 <marfogicano@gmail.com>
2024-03-15 15:44:20 -0600

    Último adelanto

---
+++ PPV2_Proyecto_Parcial2/Assets/Scripts/OptionBtm.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class OptionBtm : MonoBehaviour
+{
+    //public int OptionID;
+    //public string OptionName;
+
+    //void Start()
+    //{
+    //    transform.GetChild(0).GetComponent<TMP_Text>().text = OptionName;
+    //}
+
+
+    //public void UpdateText()
+    //{
+    //    transform.GetChild(0).GetComponent<TMP_Text>().text = OptionName;
+    //}
+
+
+    //public void SelectOption()
+    //{
+    //    LevelManager.Instance.SetPlayerAnswer(OptionID);
+    //    LevelManager.Instance.CheckPlayerState();
+    //}
+}
